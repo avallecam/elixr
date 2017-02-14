746af393e47880fbb0d7f898163e4db1c743e759
avallecam <avallecam@gmail.com>
2017-02-14 00:50:13 -0500

    Update renderRmd and no NA retrieving
    Two major updates on present commit:
    - Follow Yihui suggestions on renderRmd.
    - Major change on 02-workflow: No NA retrieving previous FILTERING

--- R/renderRmd.R
+++ R/renderRmd.R
@@ -1,13 +1,22 @@
 rm(list = ls())
 
-library(knitr)
-library(rmarkdown)
-
+#library(knitr)
 #set root dir
-opts_knit$set(root.dir='../.')
+knitr::opts_knit$set(root.dir='../.')
 
+#library(rmarkdown)
 #render
-#render("analysis/01-standardization.Rmd", clean=TRUE, output_dir= "report")
-render("analysis/02-data-filtering.Rmd", clean=TRUE, output_dir= "report")
-render("analysis/03-covariates.Rmd", clean=TRUE, output_dir= "report")
-render("analysis/04-appendix.Rmd", clean=TRUE, output_dir= "report")
+#rmarkdown::render("analysis/01-standardization.Rmd", clean=TRUE, output_dir= "report")
+rmarkdown::render("analysis/02-data-filtering.Rmd", clean=TRUE)
+rmarkdown::render("analysis/03-covariates.Rmd", clean=TRUE)
+rmarkdown::render("analysis/04-appendix.Rmd", clean=TRUE)
+
+af <- list.files(path = "./analysis", pattern = "*.png", full.names = TRUE)
+an <- list.files(path = "./analysis", pattern = "*.png", full.names = FALSE)
+b <- paste0("./figure/",an)
+file.rename(af,b)
+
+cf <- list.files(path = "./analysis", pattern = "*.html", full.names = TRUE)
+cn <- list.files(path = "./analysis", pattern = "*.html", full.names = FALSE)
+d <- paste0("./report/",cn)
+file.rename(cf,d)
