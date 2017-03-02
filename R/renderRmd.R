fc1a6a385f1ce1ca130cf4ab75e4a8706accf40a
avallecam <avallecam@gmail.com>
2017-03-02 01:15:37 -0500

    Tidy standardization output, std.uty generation, and epidemiology

--- R/renderRmd.R
+++ R/renderRmd.R
@@ -1,23 +1,29 @@
 rm(list = ls())
 
+# set root dir -----------------------------------------------------------------
+
 #library(knitr)
-#set root dir
 knitr::opts_knit$set(root.dir='../.')
 
+
+# render -----------------------------------------------------------------------
+
 #library(rmarkdown)
-#render
-#rmarkdown::render("analysis/01-standardization.Rmd", clean=TRUE)
+rmarkdown::render("analysis/01-standardization.Rmd", clean=TRUE)
 rmarkdown::render("analysis/02-data-filtering.Rmd", clean=TRUE)
 rmarkdown::render("analysis/03-covariates.Rmd", clean=TRUE)
 #rmarkdown::render("analysis/03-epidemiology.Rmd", clean=TRUE)
-#rmarkdown::render("analysis/04-appendix.Rmd", clean=TRUE)
+rmarkdown::render("analysis/00-appendix.Rmd", clean=TRUE)
 
-#move file directory path using file.rename
+# move -------------------------------------------------------------------------
+#move file directory path using file.rename 
+
 #move all .png to figure/
-af <- list.files(path = "./analysis", pattern = "*.png", full.names = TRUE)
-an <- list.files(path = "./analysis", pattern = "*.png", full.names = FALSE)
-b <- paste0("./figure/",an)
-file.rename(af,b)
+#af <- list.files(path = "./analysis", pattern = "*.png", full.names = TRUE)
+#an <- list.files(path = "./analysis", pattern = "*.png", full.names = FALSE)
+#b <- paste0("./figure/",an)
+#file.rename(af,b)
+
 #move all .html to report/
 cf <- list.files(path = "./analysis", pattern = "*.html", full.names = TRUE)
 cn <- list.files(path = "./analysis", pattern = "*.html", full.names = FALSE)
